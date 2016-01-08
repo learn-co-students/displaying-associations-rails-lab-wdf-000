65fcf8244152baf6fe36b1f04b380c3a1871a8f0
Max Cantor <max@maxcantor.net>
2016-01-07 22:41:06 -0500

    Full solution.

--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Artist, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    Artist.destroy_all
+    Song.destroy_all
+    @artist = Artist.create!(name: "Daft Punk")
+    @grid = @artist.songs.create!(title: "The Grid")
+    @voyager = @artist.songs.create!(title: "Voyager")
+  end
+
+  it "counts the songs" do
+    expect(@artist.song_count).to eq(2)
+  end
 end
--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Song, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    Artist.destroy_all
+    Song.destroy_all
+    @artist = Artist.create!(name: "Daft Punk")
+    @grid = @artist.songs.create!(title: "The Grid")
+  end
+
+  it "gets the artist name" do
+    expect(@grid.artist_name).to eq("Daft Punk")
+  end
 end
