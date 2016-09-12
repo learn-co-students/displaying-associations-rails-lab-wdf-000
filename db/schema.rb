8fad4379889bc97a40c8883298a4a16c65dc8a0b
fidelscodes <fidelscodes@gmail.com>
2016-09-12 16:15:26 -0400

    add modified schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,18 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160108015408) do
+ActiveRecord::Schema.define(version: 20160912175314) do
 
   create_table "artists", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "song_id"
   end
 
   create_table "songs", force: :cascade do |t|
     t.string   "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "artist_id"
   end
 
 end
