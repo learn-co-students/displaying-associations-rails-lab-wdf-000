3342eada0bb3f96c189aa98f4a7fec99c70196be
ozPop <ozziepopovas@gmail.com>
2016-09-13 12:27:51 -0400

    add migration, update seed.rb, migrate

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Artist.create(name: "Daft Punk")
+Song.create(title: "The Grid", artist_id: 1)
+Song.create(title: "Voyager", artist_id: 1)
