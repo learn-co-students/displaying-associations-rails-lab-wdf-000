5776743769bc27f9631928c14233a492d88683b0
Morgan VanYperen <morgvanny@gmail.com>
2018-07-02 16:07:27 -0400

    Update songs_spec.rb

--- spec/features/songs_spec.rb
+++ spec/features/songs_spec.rb
@@ -8,12 +8,12 @@ describe "songs", type:  :feature do
     @song = @artist.songs.create!(title: "The Grid")
   end
 
-  it "lists songs in 'Artist Name - Song Title' format" do
+  it "index page lists songs in 'Artist Name - Song Title' format" do
     visit songs_path
     expect(page).to have_link("Daft Punk - The Grid", href: song_path(@song))
   end
 
-  it "links to the artist" do
+  it "show page links to the artist" do
     visit song_path(@song)
     expect(page).to have_link("Daft Punk", href: artist_path(@artist))
   end
